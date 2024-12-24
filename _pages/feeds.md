ec8cc957ecf0e81f8f5f655726204136a1c1401b
maeul2 <maeul2n2@gmail.com>
2024-12-25 01:00:27 +0900

    피드 테마 만들기!!!

---
+++ _pages/feeds.md
@@ -0,0 +1,7 @@
+---
+title: Feed
+layout: feeds
+permalink: /feed/
+collection: feeds
+classes: wide
+---
---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "병원에서",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/blog/feeds/20231223.%EB%B3%91%EC%9B%90%EC%97%90%EC%84%9C",
+        "teaser": "https://github.com/user-attachments/assets/676408f8-c0a5-4322-9730-ae403c008a27"
+      },{
+        "title": "블로그를 처음 시작하며...",
+        "excerpt":"취업한지 어언 2년하고도 6개월이 지났는데… 2년 6개월 동안 거의 집 - 회사 - 테니스 루틴이었던 것 같다. 그런 와중에 부상과 테니스장 리모델링으로 일상 루틴 중 유일한 취미 생활이 중단 될 위기에 처한 상황이다! 뭐 테니스를 쉬는 동안 체형 교정도 할겸 P.T를 받을 계획이긴 하지만 예나 지금이나 헬스는 재미가 없다ㅠ 다른...","categories": [],
+        "tags": [],
+        "url": "/blog/initial-commit/",
+        "teaser": null
+      },{
+    "title": "Feed",
+    "excerpt":" ","url": "https://maeul2.github.io/blog/feed/"
+  }]
