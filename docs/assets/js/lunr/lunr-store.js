82be114489c4bcc929e546985f67ff7240b5ca9c
maeul2 <maeul2n2@gmail.com>
2024-12-28 02:31:04 +0900

    todo 리스트 만들기기

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -3,7 +3,7 @@ var store = [{
         "excerpt":"취업한지 어언 2년하고도 6개월이 지났는데… 2년 6개월 동안 거의 집 - 회사 - 테니스 루틴이었던 것 같다. 그런 와중에 부상과 테니스장 리모델링으로 일상 루틴 중 유일한 취미 생활이 중단 될 위기에 처한 상황이다! 뭐 테니스를 쉬는 동안 체형 교정도 할겸 P.T를 받을 계획이긴 하지만 예나 지금이나 헬스는 재미가 없다ㅠ 다른...","categories": [],
         "tags": [],
         "url": "/blog/initial-commit/",
-        "teaser": null
+        "teaser": "/blog/assets/images/default_teaser.png"
       },{
         "title": "병원에서",
         "excerpt":"   연 합 뉴 스!  ","categories": ["FEED"],
@@ -11,12 +11,39 @@ var store = [{
         "url": "/blog/2024/12/%EB%B3%91%EC%9B%90%EC%97%90%EC%84%9C.html",
         "teaser": "https://github.com/user-attachments/assets/676408f8-c0a5-4322-9730-ae403c008a27"
       },{
-        "title": "개발 카테고리를 처음 만들며...",
-        "excerpt":" ","categories": ["DEV","D.D.D","TODO"],
+        "title": "이게 뭘까?",
+        "excerpt":"  마을이랑 산책 중에 수풀에서 소리가 나는 곳을 마을이가 뚫어지게 보길래 뭔가 해서 봣는데 너구리인가?   처음으로 갤럭시가 아쉬웠다ㅋㅋ  ","categories": ["FEED"],
         "tags": [],
-        "url": "/blog/2024/12/%EC%9D%B4%EB%A0%A5%EA%B4%80%EB%A6%AC_%EA%B3%A0%EB%AF%BC.html",
-        "teaser": null
+        "url": "/blog/2024/12/%EB%84%88%EA%B5%AC%EB%A6%AC.html",
+        "teaser": "https://github.com/user-attachments/assets/a4c9c853-e70e-493b-84c8-44fde3a8c073"
       },{
+        "title": "혹한기 지옥 훈련",
+        "excerpt":"  내 집나간 포핸드는 언제 돌아오는걸까?  ","categories": ["FEED"],
+        "tags": [],
+        "url": "/blog/2024/12/%ED%98%B9%ED%95%9C%EA%B8%B0_%EC%A7%80%EC%98%A5_%ED%9B%88%EB%A0%A8.html",
+        "teaser": "https://github.com/user-attachments/assets/0703db94-26a8-4e67-9bc8-81bcfddb099d"
+      },{
+        "title": "개발 카테고리를 처음 만들며... 요즘 고민",
+        "excerpt":"요즘 업무적으로도 그렇고 개인 공부하면서도 그렇고 어느 한 도메인에서 일어난 이벤트들을 기록하는 기능을 어떻게 구현해야 할지 고민 중이다. 왜 하느냐? 하면… 이벤트 저장 사용자가 어떤 행위를 하였는지 추후 분석 할 수 있도록 이벤트 조회 저장 된 이벤트를 리플레이 하여 엔티티를 과거 상태로 복원 어플리케이션 계층 관점에서 보자면 이력 관리 기능이라고...","categories": ["DEV","D.D.D"],
+        "tags": ["TODO"],
+        "url": "/blog/2024/12/%EC%9D%B4%EB%A0%A5%EA%B4%80%EB%A6%AC_%EA%B3%A0%EB%AF%BC.html",
+        "teaser": "/blog/assets/images/default_teaser.png"
+      },{
+        "title": "섬진강 자전거길",
+        "excerpt":"신문의 날이 월요일!!!  ","categories": ["TODO"],
+        "tags": [],
+        "url": "/blog/2025/04/%EC%84%AC%EC%A7%84%EA%B0%95_%EC%9E%90%EC%A0%84%EA%B1%B0%EA%B8%B8.html",
+        "teaser": "/blog/assets/images/default_teaser.png"
+      },{
+        "title": "덕적도-굴업도 여행",
+        "excerpt":" ","categories": ["TODO"],
+        "tags": [],
+        "url": "/blog/2025/09/%EB%8D%95%EC%A0%81%EB%8F%84_%EA%B5%B4%EC%97%85%EB%8F%84.html",
+        "teaser": "/blog/assets/images/default_teaser.png"
+      },{
+    "title": "TODO",
+    "excerpt":"","url": "https://maeul2.github.io/blog/todo/"
+  },{
     "title": "Feed",
     "excerpt":"","url": "https://maeul2.github.io/blog/feed/index.html"
   },{
