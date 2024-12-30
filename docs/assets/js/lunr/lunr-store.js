aaf0ad4545c33d5ea5edc82fa22e9f486dd9a54a
maeul2 <maeul2n2@gmail.com>
2024-12-31 02:58:27 +0900

    deploty to docs

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,144 +2,144 @@ var store = [{
         "title": "Gallery",
         "excerpt":"                                                                                                                                                                                                                                                                                                                                                                                                     ","categories": [],
         "tags": [],
-        "url": "/pages/maeul2/gallery/1/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/gallery/1/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "Gallery",
         "excerpt":"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ","categories": [],
         "tags": [],
-        "url": "/pages/maeul2/gallery/2/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/gallery/2/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "1일 1페이지, 세상에서 가장 짧은 교양수업 365",
         "excerpt":"지금까지 읽은 내용으로 봤을 때   네 저는 지금까지 교양이 없는 사람이었고 앞으로도 교양 있는 사람이 되기는 힘들 것 같습니다…   1일 1페이지, 세상에서 가장 짧은 교양수업 365  ","categories": ["LIBRARY"],
         "tags": ["TODO"],
-        "url": "/pages/maeul2/2024/09/%EA%B5%90%EC%96%91.html",
+        "url": "/2024/09/%EA%B5%90%EC%96%91.html",
         "teaser": "https://drive.google.com/thumbnail?id=15DE-N8thxagAOH9eKjNY9DkuSrvIfzum&sz=w1000"
       },{
         "title": "블로그를 처음 시작하며...",
         "excerpt":"취업한지 어언 2년하고도 6개월이 지났는데… 2년 6개월 동안 거의 집 - 회사 - 테니스 루틴이었던 것 같다. 그런 와중에 부상과 테니스장 리모델링으로 일상 루틴 중 유일한 취미 생활이 중단 될 위기에 처한 상황이다! 뭐 테니스를 쉬는 동안 체형 교정도 할겸 P.T를 받을 계획이긴 하지만 예나 지금이나 헬스는 재미가 없다ㅠ 다른...","categories": [],
         "tags": [],
-        "url": "/pages/maeul2/initial-commit/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/initial-commit/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "병원에서",
         "excerpt":"   연 합 뉴 스!  ","categories": ["FEED"],
         "tags": [],
-        "url": "/pages/maeul2/2024/12/%EB%B3%91%EC%9B%90%EC%97%90%EC%84%9C.html",
+        "url": "/2024/12/%EB%B3%91%EC%9B%90%EC%97%90%EC%84%9C.html",
         "teaser": "https://drive.google.com/thumbnail?id=15lok8w-m1sjiz5CtB_-cM9sOLS-wrr4Q&sz=w1000"
       },{
         "title": "이게 뭘까?",
         "excerpt":"마을이랑 산책 중에 수풀에서 소리가 나는 곳을 마을이가 뚫어지게 보길래 뭔가 해서 봣는데…     너구리인가? 여우는 아니겟지ㅎㅎ   처음으로 갤럭시가 아쉬웠다ㅋㅋ  ","categories": ["FEED"],
         "tags": [],
-        "url": "/pages/maeul2/2024/12/%EB%84%88%EA%B5%AC%EB%A6%AC.html",
+        "url": "/2024/12/%EB%84%88%EA%B5%AC%EB%A6%AC.html",
         "teaser": "https://drive.google.com/thumbnail?id=15kauv4PBBOMEs774kuekYSNjGL28YlDh&sz=w1000"
       },{
         "title": "이토록 다정한 개인주의자",
         "excerpt":"이 책을 일게 된 계기는 책의 목차에서 ‘고속버스 좌석 등받이를 마음대로 젖혀도 괜찮을까?’ 라는 제목을 보고 흥미가 동해서 이다. (최근에 비행기나 고속 버스에서 등받이를 젖혀도 되느냐 마냐로 논란이 일었다.) 이 외에도 책에서는 기냐 아니냐로 논란이 일었던 사건들을 다루고 있다. 과연 이 책에서는 어떤 입장의 손을 들어줄까? 라는 생각을 하며 책을...","categories": ["LIBRARY"],
         "tags": [],
-        "url": "/pages/maeul2/2024/12/%EA%B0%9C%EC%9D%B8%EC%A3%BC%EC%9D%98%EC%9E%90.html",
+        "url": "/2024/12/%EA%B0%9C%EC%9D%B8%EC%A3%BC%EC%9D%98%EC%9E%90.html",
         "teaser": "https://drive.google.com/thumbnail?id=15GLNkvL4W2YIUxDxoCAScHdPUoy8wjIs&sz=w1000"
       },{
         "title": "혹한기 지옥 훈련",
         "excerpt":"  내 집나간 포핸드는 언제 돌아오는걸까?  ","categories": ["FEED"],
         "tags": [],
-        "url": "/pages/maeul2/2024/12/%ED%98%B9%ED%95%9C%EA%B8%B0_%EC%A7%80%EC%98%A5_%ED%9B%88%EB%A0%A8.html",
+        "url": "/2024/12/%ED%98%B9%ED%95%9C%EA%B8%B0_%EC%A7%80%EC%98%A5_%ED%9B%88%EB%A0%A8.html",
         "teaser": "https://drive.google.com/thumbnail?id=15Qt8ULBmsdmzzGPQVZA-KAfi1n1Ip5Aw&sz=w1000"
       },{
         "title": "개발 카테고리를 처음 만들며... 요즘 고민",
         "excerpt":"요즘 업무에서도 그렇고 개인 공부하면서도 그렇고 어느 한 도메인에서 일어난 이벤트들을 기록하는 기능을 어떻게 구현해야 할지 고민 중이다. 왜 하느냐? 하면… 이벤트 저장 사용자가 어떤 행위를 하였는지 추후 분석 할 수 있도록 이벤트 조회 저장 된 이벤트를 리플레이 하여 엔티티를 과거 상태로 복원 어플리케이션 계층 관점에서 보자면 이력 관리 기능이라고...","categories": ["DEV","D.D.D"],
         "tags": ["TODO"],
-        "url": "/pages/maeul2/2024/12/%EC%9D%B4%EB%A0%A5%EA%B4%80%EB%A6%AC_%EA%B3%A0%EB%AF%BC.html",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/2024/12/%EC%9D%B4%EB%A0%A5%EA%B4%80%EB%A6%AC_%EA%B3%A0%EB%AF%BC.html",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "섬진강 자전거길",
         "excerpt":"신문의 날이 월요일!!!   섬진강 벚꽃길 자전거 여행  ","categories": [],
         "tags": ["여행"],
-        "url": "/pages/maeul2/todo/2024-12-27-%EC%84%AC%EC%A7%84%EA%B0%95_%EC%9E%90%EC%A0%84%EA%B1%B0%EA%B8%B8/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-27-%EC%84%AC%EC%A7%84%EA%B0%95_%EC%9E%90%EC%A0%84%EA%B1%B0%EA%B8%B8/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "덕적도-굴업도 여행",
         "excerpt":"마을이와 함께하는 섬여행   덕적도에서 낚시하고 굴업도에서 백패킹.  ","categories": [],
         "tags": ["여행"],
-        "url": "/pages/maeul2/todo/2024-12-27-%EB%8D%95%EC%A0%81%EB%8F%84_%EA%B5%B4%EC%97%85%EB%8F%84/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-27-%EB%8D%95%EC%A0%81%EB%8F%84_%EA%B5%B4%EC%97%85%EB%8F%84/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "드럼 배우기",
         "excerpt":"드럼을 배우고 언젠가 마음 맞는 사람들이랑 밴드 활동도 해보고 싶다.  ","categories": [],
         "tags": ["취미"],
-        "url": "/pages/maeul2/todo/2024-12-27-%EB%93%9C%EB%9F%BC/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-27-%EB%93%9C%EB%9F%BC/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "바둑 배우기",
         "excerpt":" ","categories": [],
         "tags": ["취미"],
-        "url": "/pages/maeul2/todo/2024-12-27-%EB%B0%94%EB%91%91/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-27-%EB%B0%94%EB%91%91/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "겨울 몽골? 아이슬란드? 여행",
         "excerpt":"20대에 느꼈던 몽골 or 아이슬란드가 30대에는 느낌일까?   과연 갈까?   ","categories": [],
         "tags": ["여행"],
-        "url": "/pages/maeul2/todo/2024-12-28-%EB%AA%BD%EA%B3%A8_%EC%95%84%EC%9D%B4%EC%8A%AC%EB%9E%80%EB%93%9C_%EC%97%AC%ED%96%89/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-28-%EB%AA%BD%EA%B3%A8_%EC%95%84%EC%9D%B4%EC%8A%AC%EB%9E%80%EB%93%9C_%EC%97%AC%ED%96%89/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "이번엔 꼭 일본 가자",
         "excerpt":"오사카? 교토? 도쿄는 별로…  ","categories": [],
         "tags": ["여행"],
-        "url": "/pages/maeul2/todo/2024-12-31-%EC%9D%BC%EB%B3%B8_%EC%97%AC%ED%96%89/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-31-%EC%9D%BC%EB%B3%B8_%EC%97%AC%ED%96%89/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "캐나다 여행",
         "excerpt":"퀘백, 옐로나이프…오로라!!!  ","categories": [],
         "tags": ["여행"],
-        "url": "/pages/maeul2/todo/2024-12-28-%EC%BA%90%EB%82%98%EB%8B%A4_%EC%97%AC%ED%96%89/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-28-%EC%BA%90%EB%82%98%EB%8B%A4_%EC%97%AC%ED%96%89/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "북유럽 여행",
         "excerpt":"피요르드, 트레킹…젊을 때 고생해야지  ","categories": [],
         "tags": ["여행"],
-        "url": "/pages/maeul2/todo/2024-12-29-%EB%B6%81%EC%9C%A0%EB%9F%BD_%EC%97%AC%ED%96%89/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-29-%EB%B6%81%EC%9C%A0%EB%9F%BD_%EC%97%AC%ED%96%89/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
         "title": "뉴질랜드 여행",
         "excerpt":"10년차 근속 휴가. 이 때가 오긴 할까ㅋㅋ  ","categories": [],
         "tags": ["여행"],
-        "url": "/pages/maeul2/todo/2024-12-29-%EB%89%B4%EC%A7%88%EB%9E%9C%EB%93%9C_%EC%97%AC%ED%96%89/",
-        "teaser": "/pages/maeul2/assets/images/default_teaser.png"
+        "url": "/todo/2024-12-29-%EB%89%B4%EC%A7%88%EB%9E%9C%EB%93%9C_%EC%97%AC%ED%96%89/",
+        "teaser": "/assets/images/default_teaser.png"
       },{
     "title": "TODO",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/todo/"
+    "excerpt":"","url": "https://maeul2.github.io/todo/"
   },{
     "title": "Gallery",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/gallery/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/gallery/index.html"
   },{
     "title": "Gallery",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/gallery/page/2/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/gallery/page/2/index.html"
   },{
     "title": "Feed",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/feed/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/feed/index.html"
   },{
     "title": "Library",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/library/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/library/index.html"
   },{
     "title": "Spring Security",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/spring-security/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/spring-security/index.html"
   },{
     "title": "Spring",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/spring/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/spring/index.html"
   },{
     "title": "MICROSERVICE",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/microservice/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/microservice/index.html"
   },{
     "title": "Design Pattern",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/design-pattern/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/design-pattern/index.html"
   },{
     "title": "Domain Driven Design",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/d-d-d/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/d-d-d/index.html"
   },{
     "title": "Dev",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/dev/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/dev/index.html"
   },{
     "title": "마을이네",
-    "excerpt":"","url": "https://maeul2.github.io/pages/maeul2/index.html"
+    "excerpt":"","url": "https://maeul2.github.io/index.html"
   }]
